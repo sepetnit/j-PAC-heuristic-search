1a5fa31c4c7c9a6708c1f7ad1e1b68b93718713f
Gal <Dreiman>
2017-02-26 00:53:05 +0200

    create a base experiment runner and 2 subclasses:
    one for preprocess and the other for online search

--- src/main/java/og/cs4j/core/experiments/ExperimentRunner.java
+++ src/main/java/og/cs4j/core/experiments/ExperimentRunner.java
@@ -1,4 +1,4 @@
-package org.cs4j.core.mains;
+package og.cs4j.core.experiments;
 
 import org.cs4j.core.OutputResult;
 import org.cs4j.core.SearchAlgorithm;
@@ -17,90 +17,18 @@ import java.util.Map;
 /**
  * Created by user on 23/02/2017.
  */
-public class ExperimentRunner {
-    public double[] run(Class domainClass, SearchAlgorithm alg,
-                                  String inputPath,
-                                  String outputPath,
-                                  int startInstance,
-                                  int stopInstance,
-                                  HashMap<String,String> domainParams,
-                                  boolean overwriteFile){
-        double retArray[] = {0,0,0};//solved,generated,expanded
-        String[] resultColumnNames = {"InstanceID", "Found", "Depth", "Cost" , "Generated", "Expanded", "Cpu Time", "Wall Time"};
-        OutputResult output = null;
-        double[] resultsData;
-        SearchDomain domain;
-        SearchResult result;
-        int solvableNum = stopInstance-startInstance+1;
-        boolean[] solvableInstances = new boolean[solvableNum];
-        Arrays.fill(solvableInstances,true);
-
-        Constructor<?> cons = getSearchDomainConstructor(domainClass);
-        try {
-            // Write headers of output table
-            output = new OutputResult(outputPath+alg.getName(), null, -1, -1, null, false, overwriteFile);
-            String toPrint = String.join(",", resultColumnNames);
-            output.writeln(toPrint);
-
-            System.out.println("Solving "+domainClass.getName() + "\tAlg: " + alg.getName());
-            int found = 0;
-            //search on this domain and algo and weight the 100 instances
-            for (int i = startInstance; i <= stopInstance; ++i) {
-                try {
-                    // Read domain from file
-                    resultsData = new double[resultColumnNames.length];
-                    domain = getSearchDomain(inputPath, domainParams, cons, i);
-
-                    System.out.print("\rSolving " + domainClass.getName() + " instance " + (found+1)
-                            +"/"+ i +" ("+solvableNum+")\tAlg: " + alg.getName());
-                    result = null;
-                    if(solvableInstances[i-startInstance])
-                        result = alg.search(domain);
-                    if (result != null && result.hasSolution()) {
-                        found++;
-                        setResultsData(resultsData, result);
-                    }
-                    else if(solvableInstances[i-startInstance]){
-                        solvableInstances[i-startInstance] = false;
-                        solvableNum--;
-
-                        int sul = 0;
-                        for (boolean solvableInstance : solvableInstances) {
-                            if (solvableInstance)
-                                sul++;
-                        }
-                        if(sul!= solvableNum)
-                            System.out.println("[WARNING] solvable num incorrect i:"+i);
-                    }
-                    resultsData[0] = i;
-                    output.appendNewResult(resultsData);
-                    output.newline();
-                } catch (OutOfMemoryError e) {
-                    System.out.println("[INFO] MainDaniel OutOfMemory :-( "+e);
-                    System.out.println("[INFO] OutOfMemory in:"+alg.getName()+" on:"+ domainClass.getName());
-                }
-                catch (FileNotFoundException e) {
-                    System.out.println("[INFO] FileNotFoundException At inputPath:"+inputPath);
-                    i=stopInstance; // @TODO: Is this just a replacement for break?
-                }
-            }
-        }  catch (IOException e) {
-            System.out.println("[INFO] IOException At outputPath:"+outputPath);
-            e.printStackTrace();
-        }
-        finally {
-            output.close();
-        }
-        return retArray;
-    }
-
+public abstract class ExperimentRunner {
+   
+	
+	public abstract double[] run(Class domainClass, SearchAlgorithm alg, String inputPath, String outputPath, int startInstance,
+			int stopInstance, HashMap<String, String> domainParams, boolean overwriteFile);
 
     /**
      * Extract from SearchResults the data to output to the file
      * @param resultsData the results data object to update with data from the SearchResults
      * @param result the SearchResults object to extract from
      */
-    private void setResultsData(double[] resultsData, SearchResult result) {
+	protected void setResultsData(double[] resultsData, SearchResult result) {
         resultsData[1] = 1;
         resultsData[2] = result.getSolutions().get(0).getLength();
         resultsData[3] = result.getSolutions().get(0).getCost();
@@ -114,7 +42,7 @@ public class ExperimentRunner {
      * Create a constructor for the search domain, to enable creating search domains for a 
      * @param domainClass the class of the domain
      */
-    private Constructor<?> getSearchDomainConstructor(Class domainClass) {
+    protected Constructor<?> getSearchDomainConstructor(Class domainClass) {
         Constructor<?> cons = null;
         try {
             cons = domainClass.getConstructor(InputStream.class);
@@ -133,7 +61,7 @@ public class ExperimentRunner {
      * @return A SearchDomain object
      *
      */
-    private SearchDomain getSearchDomain(String inputPath, HashMap<String, String> domainParams, Constructor<?> cons, int i){
+    protected SearchDomain getSearchDomain(String inputPath, HashMap<String, String> domainParams, Constructor<?> cons, int i){
         InputStream is = null;
         SearchDomain domain=null;
         try {
@@ -255,20 +183,5 @@ public class ExperimentRunner {
     }
 
 
-    /**
-     * An example of using ExperimentRunner
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        ExperimentRunner runner = new ExperimentRunner();
-        SearchAlgorithm algorithm = new AnytimePTS();
-        HashMap domainParams = new HashMap<>();
 
-        runner.run(FifteenPuzzle.class,algorithm,
-                "./input/FifteenPuzzle/states15",
-                "./results/FifteenPuzzle/anytime",
-                1,100,
-                domainParams,true);
-    }
 }
